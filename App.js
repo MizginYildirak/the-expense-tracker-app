ea267a7f363d551db6f9a3d8111e2de69e607c10
MizginYildirak <mizgin_yldrak@hotmail.com>
2025-02-01 15:58:42 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import ManageExpense from "./screens/ManageExpense";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+    </BottomTabs.Navigator>
+  );
+}
---
+++ screens/ManageExpense.js
@@ -0,0 +1,7 @@
+import {Text} from "react-native"
+
+function ManageExpenses(params) {
+    return <Text>ManageExpenses Screen</Text>
+}
+
+export default ManageExpenses
